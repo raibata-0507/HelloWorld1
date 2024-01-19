91350a5193f66a15c3542f8c6a9a386677d6ebe3
raibata-0507 <rsb2021085@sicsr.ac.in>
2024-01-19 16:55:30 +0530

    added HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Java for Jenkins and Ngrok");
+    }
+}
